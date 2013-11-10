7e6a34728854da40383d7dbc71ea640ea11f75cb
Angelo <amsd@netc.pt>
2013-11-10 15:49:25 +0000

    1

---
+++ WebApplication1/Controllers/CampusController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CampusController : Controller
+    {
+        //
+        // GET: /Campus/
+        public ActionResult Faculty()
+        {
+            return View();
+        }
+	}
+}
---
+++ WebApplication1/Controllers/MapController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class MapController : Controller
+    {
+        //
+        // GET: /Map/
+        public ActionResult Gambelas()
+        {
+            return View();
+        }
+        public ActionResult Penha()
+        {
+            return View();
+        }
+        public ActionResult ESS()
+        {
+            return View();
+        }
+        public ActionResult Portimao()
+        {
+            return View();
+        }
+	}
+}
